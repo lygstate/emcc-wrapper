fbadb1c8d4fb7990466b05983a7161cc88134bee
Yonggang Luo <luoyonggang@gmail.com>
2026-02-09 20:16:34 +0800

    Now do not call python directly

--- wrapper.c
+++ wrapper.c
@@ -4,213 +4,201 @@
  * University of Illinois/NCSA Open Source License.  Both these licenses can be
  * found in the LICENSE file.
  *
- * Small win32 application that is used to launcher emscripten via python.exe.
+ * Small win32 application that is used to launcher emscripten via python3.dll.
  * On non-windows platforms this is done via the run_pyton.sh shell script.
  *
  * The binary will look for a python script that matches its own name and run
- * that using python.exe.
+ * that using python3.dll.
  */
 
 // Define _WIN32_WINNT to Windows 7 for max portability
 #define _WIN32_WINNT 0x0601
 
-#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdlib.h>
 
 #include <windows.h>
 
-#include <shellapi.h>
-#include <shlwapi.h>
+#include <minwindef.h>
+#include <wchar.h>
 
-#pragma comment(lib, "shlwapi.lib")
-#pragma comment(lib, "shell32.lib")
+typedef int (*Py_MainFunction)(int argc, wchar_t** argv);
 
-wchar_t* get_python_executable() {
-  wchar_t* python_exe_w = _wgetenv(L"EMSDK_PYTHON");
-  if (!python_exe_w) {
-    return L"python.exe";
+#define malloc malloc_local
+#define realloc realloc_local
+#define wcslen wcslen_local
+#define wcsdup wcsdup_local
+#define memcpy memcpy_local
+#define free free_local
+
+static void* malloc(size_t _Size) {
+  return HeapAlloc(GetProcessHeap(), 0, _Size);
+}
+
+static void* realloc(void* _Block, size_t _Size) {
+  if (_Block == NULL)
+    return malloc(_Size);
+  return HeapReAlloc(GetProcessHeap(), 0, _Block, _Size);
+}
+
+static void free(void* _Block) {
+  HeapFree(GetProcessHeap(), 0, _Block);
+}
+
+static size_t wcslen(const wchar_t* String) {
+  size_t len = 0;
+  while (String[len] > 0)
+    len += 1;
+  return len;
+}
+
+static void* memcpy(void* _Dst, void const* _Src, size_t _Size) {
+  uint8_t* dst = (uint8_t*)_Dst;
+  uint8_t* src = (uint8_t*)_Src;
+  for (size_t i = 0; i < _Size; ++i) {
+    dst[i] = src[i];
   }
-  return python_exe_w;
+  return dst;
 }
 
-wchar_t* get_long_path(const wchar_t* path) {
-  DWORD path_len = GetFullPathNameW(path, 0, NULL, NULL);
-  if (path_len == 0)
-    abort();
-
-  wchar_t* full_path = malloc(sizeof(wchar_t) * (path_len + 1));
-  if (!full_path)
-    abort();
-
-  full_path[path_len] = 0;
-  if (GetFullPathNameW(path, path_len, full_path, NULL) == 0)
-    abort();
-  return full_path;
+static wchar_t* wcsdup(const wchar_t* String) {
+  size_t len = wcslen(String);
+  size_t alloca_size = sizeof(wchar_t) * (len + 1);
+  wchar_t* new_string = malloc(alloca_size);
+  memcpy(new_string, String, alloca_size);
+  return new_string;
 }
 
-// Get the name of the currently running executable (module)
-wchar_t* get_module_path() {
-  DWORD buffer_size = MAX_PATH;
-  wchar_t* module_path_w = malloc(sizeof(wchar_t) * buffer_size);
-  if (!module_path_w)
-    abort();
+typedef DWORD (*windows_get_buffer_api_callback)(const void* context,
+                                                 wchar_t* buffer,
+                                                 DWORD buffer_size);
 
-  DWORD path_len = GetModuleFileNameW(NULL, module_path_w, buffer_size);
-  while (path_len > 0 && path_len == buffer_size) {
-    buffer_size *= 2;
-    module_path_w = realloc(module_path_w, sizeof(wchar_t) * buffer_size);
-    if (!module_path_w)
-      abort();
-    path_len = GetModuleFileNameW(NULL, module_path_w, buffer_size);
-  }
-
-  if (path_len == 0)
-    abort();
-
-  return module_path_w;
-}
-
-// Handle the _EMCC_CCACHE environment variable which essentially re-executes
-// the lauccher command with `cacche.exe` prefixed.
-BOOL handle_ccache(wchar_t** application_name,
-                   wchar_t** command_line,
-                   const wchar_t* launcher_path) {
-  const wchar_t* ccache_env = _wgetenv(L"_EMCC_CCACHE");
-  if (!ccache_env || !*ccache_env) {
-    return FALSE;
-  }
-
-  _wputenv_s(L"_EMCC_CCACHE", L"");
-  *application_name = L"ccache.exe";
-
-  size_t command_line_len =
-      wcslen(*application_name) + wcslen(launcher_path) + 6;
-  *command_line = malloc(sizeof(wchar_t) * command_line_len);
-  if (!*command_line)
-    abort();
-
-  swprintf(*command_line, command_line_len, L"\"%s\" \"%s\"", *application_name,
-           launcher_path);
-  return TRUE;
-}
-
-// A custom replacement for PathGetArgsW that is safe for command lines
-// longer than MAX_PATH.
-const wchar_t* find_args(const wchar_t* command_line) {
-  const wchar_t* p = command_line;
-
-  // Skip past the executable name, which can be quoted.
-  if (*p == L'"') {
-    // The path is quoted, find the closing quote.
-    p++;
-    while (*p) {
-      if (*p == L'"') {
-        p++;
+static wchar_t* call_windows_get_buffer_api(windows_get_buffer_api_callback api,
+                                            const void* context) {
+  wchar_t* buffer = NULL;
+  DWORD size = api(context, NULL, 0);
+  if (size > 0) {
+    // The size fetched, directly use it
+    DWORD buffer_size = size;
+    buffer = malloc(buffer_size * sizeof(wchar_t));
+    size = api(context, buffer, buffer_size);
+    if (size + 1 != buffer_size) {
+      free(buffer);
+      buffer = NULL;
+    }
+  } else {
+    DWORD err;
+    // The size can not be fetched when err is ERROR_INSUFFICIENT_BUFFER, then
+    // start with a basic bufer size, dynamically double buffer size when
+    // needed.
+    DWORD buffer_size = 64;
+    for (; (err = GetLastError()) == ERROR_INSUFFICIENT_BUFFER;) {
+      buffer_size *= 2;
+      buffer = realloc(buffer, buffer_size * sizeof(wchar_t));
+      if (buffer == NULL) {
         break;
       }
-      p++;
+      size = api(context, buffer, buffer_size);
     }
-  } else {
-    // The path is not quoted, find the first space.
-    while (*p && *p != L' ' && *p != L'\t') {
-      p++;
+    if (buffer != NULL) {
+      if (err == ERROR_SUCCESS) {
+        buffer[size] = 0;
+        if ((size + 1) != buffer_size) {
+          buffer = realloc(buffer, (size + 1) * sizeof(wchar_t));
+        }
+      } else {
+        free(buffer);
+        buffer = NULL;
+      }
     }
   }
-
-  // Skip any whitespace between the executable and the first argument.
-  while (*p && (*p == L' ' || *p == L'\t')) {
-    p++;
-  }
-
-  return p;
+  return buffer;
 }
 
-// Create the script path by taking the launcher path and replacing the
-// extension with .py. For example `C:\path\to\emcc.exe` becomes
-// `C:\path\to\emcc.py`.
-wchar_t* get_script_path(const wchar_t* launcher_path) {
-  wchar_t* script_path_w = wcsdup(launcher_path);
-  if (!script_path_w)
-    abort();
-  PathRemoveExtensionW(script_path_w);
-  size_t current_len = wcslen(script_path_w);
-  // 4 for `.py` and the null terminator.
-  size_t new_size_in_chars = current_len + 4;
-  script_path_w = realloc(script_path_w, new_size_in_chars * sizeof(wchar_t));
-  if (!script_path_w)
-    abort();
-  wcscat_s(script_path_w, new_size_in_chars, L".py");
-  return script_path_w;
+DWORD GetModuleFileNameW_callback(const void* context,
+                                  wchar_t* buffer,
+                                  DWORD buffer_size) {
+  HMODULE hmodule = (HMODULE)context;
+  return GetModuleFileNameW(hmodule, buffer, buffer_size);
 }
 
-int wmain(int argc, wchar_t** argv) {
-  const wchar_t* ccache = NULL;
-  const wchar_t* ccache_env = _wgetenv(L"_EMCC_CCACHE");
-  if (ccache_env && ccache_env[0]) {
-    ccache = L"ccache.exe";
-  }
+static inline wchar_t* get_module_path() {
+  return call_windows_get_buffer_api(GetModuleFileNameW_callback, NULL);
+}
 
-  wchar_t* application_name = get_python_executable();
-  wchar_t* launcher_path_w = get_module_path();
-  wchar_t* script_path_w = get_script_path(launcher_path_w);
-  wchar_t* long_script_path = get_long_path(script_path_w);
-  size_t command_line_len =
-      wcslen(application_name) + wcslen(long_script_path) + 9;
-  if (ccache) {
-    command_line_len += wcslen(ccache) + 1;
-  }
-  wchar_t* command_line = malloc(sizeof(wchar_t) * command_line_len);
-  if (ccache) {
-    swprintf(command_line, command_line_len, L"%s \"%s\" -E \"%s\"", ccache,
-             application_name, long_script_path);
-  } else {
-    swprintf(command_line, command_line_len, L"\"%s\" -E \"%s\"",
-             application_name, long_script_path);
-  }
+DWORD GetEnvironmentVariableW_callback(const void* context,
+                                       wchar_t* buffer,
+                                       DWORD buffer_size) {
+  const wchar_t* name = (const wchar_t*)context;
+  return GetEnvironmentVariableW(name, buffer, buffer_size);
+}
 
-  free(long_script_path);
+static inline wchar_t* get_environment_variable(wchar_t* name) {
+  return call_windows_get_buffer_api(GetEnvironmentVariableW_callback, name);
+}
+
+DWORD GetFullPathNameW_callback(const void* context,
+                                wchar_t* buffer,
+                                DWORD buffer_size) {
+  const wchar_t* path = (const wchar_t*)context;
+  return GetFullPathNameW(path, buffer_size, buffer, NULL);
+}
+
+static inline wchar_t* get_long_path(const wchar_t* path) {
+  return call_windows_get_buffer_api(GetFullPathNameW_callback, path);
+}
+
+wchar_t* get_python_dll() {
+  wchar_t* python_dll_w = get_environment_variable(L"EMSDK_PYTHON_DLL");
+  if (!python_dll_w) {
+    return wcsdup(L"python3.dll");
+  }
+  return python_dll_w;
+}
+
+void wmainCRTStartup() {
   // -E will not ignore _PYTHON_SYSCONFIGDATA_NAME an internal
   // of cpython used in cross compilation via setup.py.
-  _wputenv_s(L"_PYTHON_SYSCONFIGDATA_NAME", L"");
+  SetEnvironmentVariableW(L"_PYTHON_SYSCONFIGDATA_NAME", L"");
 
-  // Build the final command line by appending the original arguments
-  const wchar_t* all_args = find_args(GetCommandLineW());
-  if (all_args && *all_args) {
-    size_t current_len = wcslen(command_line);
-    size_t args_len = wcslen(all_args);
-    // +2 for the space and the null terminator
-    command_line =
-        realloc(command_line, (current_len + args_len + 2) * sizeof(wchar_t));
-    if (!command_line)
-      abort();
-    wcscat_s(command_line, current_len + args_len + 2, L" ");
-    wcscat_s(command_line, current_len + args_len + 2, all_args);
-  }
-
-  // Work around python bug 34780 by closing stdin, so that it is not inherited
-  // by the python subprocess.
-  wchar_t* workaround_env = _wgetenv(L"EM_WORKAROUND_PYTHON_BUG_34780");
+  // Work around python bug 34780 by closing stdin, so that it is not
+  // inherited by the python subprocess.
+  wchar_t* workaround_env =
+      get_environment_variable(L"EM_WORKAROUND_PYTHON_BUG_34780");
   if (workaround_env) {
     CloseHandle(GetStdHandle(STD_INPUT_HANDLE));
   }
 
-  STARTUPINFOW si;
-  PROCESS_INFORMATION pi;
-  ZeroMemory(&si, sizeof(si));
-  si.cb = sizeof(si);
-  ZeroMemory(&pi, sizeof(pi));
+  wchar_t* launcher_path_w = get_module_path();
+  size_t launcher_path_w_length = wcslen(launcher_path_w);
+  // Change the .exe extension to .py
+  memcpy(launcher_path_w + launcher_path_w_length - 3, L"py", 6);
+  wchar_t* long_script_path = get_long_path(launcher_path_w);
+  free(launcher_path_w);
 
-  if (!CreateProcessW(application_name, command_line, NULL, NULL, FALSE, 0,
-                      NULL, NULL, &si, &pi)) {
-    wprintf(L"CreateProcess failed (%d).\n", GetLastError());
-    abort();
+  // Build the final command line by appending the original arguments
+  int argc;
+  wchar_t** argv = CommandLineToArgvW(GetCommandLineW(), &argc);
+  wchar_t** argv_final = malloc(((argc + 2) * sizeof(wchar_t*)));
+  argv_final[0] = argv[0];
+  argv_final[1] = L"-E";
+  argv_final[2] = long_script_path;
+  for (int i = 1; i < argc; i += 1) {
+    argv_final[2 + i] = argv[i];
   }
 
-  WaitForSingleObject(pi.hProcess, INFINITE);
+  wchar_t* python_application_dll = get_python_dll();
+  HMODULE python_dll_hmodule = LoadLibraryW(python_application_dll);
+  Py_MainFunction Py_Main =
+      (Py_MainFunction)GetProcAddress(python_dll_hmodule, "Py_Main");
+  free(python_application_dll);
 
-  DWORD exit_code;
-  GetExitCodeProcess(pi.hProcess, &exit_code);
-  CloseHandle(pi.hProcess);
-  CloseHandle(pi.hThread);
-  return exit_code;
+  int ret = Py_Main(argc + 2, argv_final);
+  FreeLibrary(python_dll_hmodule);
+
+  free(long_script_path);
+  free(argv);
+  free(argv_final);
+  ExitProcess(ret);
 }
